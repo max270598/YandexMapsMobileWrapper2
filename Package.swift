8c86cf2e691b79b11931520750f5cd5761c7f8ee
MaxOl <some@mail.com>
2022-07-11 09:04:30 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "YandexMapsMobileWrapper",
-            type: .static,
             // Используем таргет обертку над XCFramework и его зависимостями
             targets: ["YandexMapsMobileWrapper"]),
     ],
@@ -20,12 +19,12 @@ let package = Package(
     targets: [
         // Подключаем наш фреймворк локально, также его можно добавлять и через url
 //        .binaryTarget(name: "YandexMapsMobileBinary", path: "./Sources/YandexMapsMobile.xcframework"),
-        .binaryTarget(name: "YandexMapsMobileLiteSPM", url: "https://github.com/crownvic/YandexMapsMobileLiteSPM/releases/download/4.1.0/YandexMapsMobile-4.1.0-lite.framework.zip", checksum: "28310d0c2661625b8137e41b2327f751c3a25d8aa389e6772990a73e39e6485c"),
+        .binaryTarget(name: "YandexMapsMobile-4.1.0-lite", url: "https://github.com/crownvic/YandexMapsMobileLiteSPM/releases/download/4.1.0/YandexMapsMobile-4.1.0-lite.framework.zip", checksum: "28310d0c2661625b8137e41b2327f751c3a25d8aa389e6772990a73e39e6485c"),
         // обертываем наш фреймворк зависимостями
         .target(
             name: "YandexMapsMobileWrapper",
             dependencies: [
-                .target(name: "YandexMapsMobileLiteSPM"),
+                .target(name: "YandexMapsMobile-4.1.0-lite"),
             ],
             linkerSettings: [
                 .linkedFramework("CoreLocation"),
