4616c3a3403a78bd4918b6d7986a85334c87b263
MaxOl <some@mail.com>
2022-07-11 08:43:37 +0300

    crownic fix

--- Package.swift
+++ Package.swift
@@ -20,12 +20,12 @@ let package = Package(
     targets: [
         // Подключаем наш фреймворк локально, также его можно добавлять и через url
 //        .binaryTarget(name: "YandexMapsMobileBinary", path: "./Sources/YandexMapsMobile.xcframework"),
-        .binaryTarget(name: "YandexMapsMobile", url: "https://github.com/max270598/YandexMapsMobileZip/releases/download/2.0.0/YandexMapsMobile.xcframework.zip", checksum: "2af1813d655ac552f84fb3e733cbae8aec8428db69081289cae357278641efc8"),
+        .binaryTarget(name: "YandexMapsMobileLiteSPM", url: "https://github.com/crownvic/YandexMapsMobileLiteSPM/releases/download/4.1.0/YandexMapsMobile-4.1.0-lite.framework.zip", checksum: "28310d0c2661625b8137e41b2327f751c3a25d8aa389e6772990a73e39e6485c"),
         // обертываем наш фреймворк зависимостями
         .target(
             name: "YandexMapsMobileWrapper",
             dependencies: [
-                .target(name: "YandexMapsMobile"),
+                .target(name: "YandexMapsMobileLiteSPM"),
             ],
             linkerSettings: [
                 .linkedFramework("CoreLocation"),
